700ed196ecb173985ac0a54358f23be3620e76d0
Jona <llamasjonat@gmail.com>
2024-10-10 22:58:01 -0300

    feat: added Nostr context

---
+++ src/context/Nostr.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import Link from 'next/link';
+import { useNostrHooks, useAutoLogin, useActiveUser, useLogin } from 'nostr-hooks';
+
+import { ndk } from '@/lib/nostr';
+
+export const NostrContext = ({ children }: any) => {
+  useNostrHooks(ndk);
+  useAutoLogin();
+
+  const { loginWithExtension } = useLogin();
+  const { activeUser } = useActiveUser();
+
+  return (
+    <div>
+      <header className='border-b'>
+        <div className='container mx-auto px-4 py-4 flex items-center justify-between'>
+          <h1 className='text-2xl font-bold text-primary'>NostrEvents</h1>
+          <nav className='hidden md:flex space-x-4'>
+            <Link href='/' className='text-muted-foreground hover:text-primary'>
+              Home
+            </Link>
+            <Link href='/home' className='text-muted-foreground hover:text-primary'>
+              Events
+            </Link>
+            <Link href='/create' className='text-muted-foreground hover:text-primary'>
+              Create
+            </Link>
+
+            {activeUser ? (
+              <p>Bienvenido</p>
+            ) : (
+              <button onClick={() => loginWithExtension()}>Login with Extension</button>
+            )}
+          </nav>
+        </div>
+      </header>
+      {children}
+    </div>
+  );
+};
