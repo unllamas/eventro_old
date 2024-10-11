1c4fcf64f0e7780d3219a6040445bff40852616e
Jona <llamasjonat@gmail.com>
2024-10-10 22:58:07 -0300

    feat: added Nostr context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 
 import '@/style/globals.css';
+import { NostrContext } from '@/context/Nostr';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -14,7 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`antialiased`}>{children}</body>
+      <body className={`antialiased`}>
+        <NostrContext>{children}</NostrContext>
+      </body>
     </html>
   );
 }
