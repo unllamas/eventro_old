f9a320ba55ecd2844fa1ee932a4f30055875e3df
Jona <llamasjonat@gmail.com>
2024-10-11 21:00:29 -0300

    feat: added Event and Events API

--- src/lib/nostr.ts
+++ src/lib/nostr.ts
@@ -1,6 +1,26 @@
-import NDK from '@nostr-dev-kit/ndk';
+import NDK, { NDKEvent, NDKFilter } from '@nostr-dev-kit/ndk';
 
 // Nostr
 export const RELAYS = ['wss://relay.primal.net/'];
 
 export const ndk = new NDK({ explicitRelayUrls: RELAYS });
+
+export async function connectToNDK(): Promise<NDK> {
+  await ndk.connect();
+
+  return ndk;
+}
+
+export async function fetchNostrEvent(filter: NDKFilter): Promise<NDKEvent | null> {
+  const ndk = await connectToNDK();
+  const event = await ndk.fetchEvent(filter);
+
+  return event || null;
+}
+
+export async function fetchNostrEvents(filter: NDKFilter): Promise<NDKEvent[]> {
+  const ndk = await connectToNDK();
+  const events = await ndk.fetchEvents(filter);
+
+  return Array.from(events);
+}
