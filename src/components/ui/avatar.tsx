bd65da31e15ac0181b77ed0189436c5f6d9a2c56
Jona <llamasjonat@gmail.com>
2024-10-15 11:32:17 -0300

    fix(component): update Avatar

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -1,50 +1,50 @@
-"use client"
+import Image from 'next/image';
 
-import * as React from "react"
-import * as AvatarPrimitive from "@radix-ui/react-avatar"
+import { cn } from '@/lib/utils';
 
-import { cn } from "@/lib/utils"
+import { Skeleton } from '@/components/ui/skeleton';
 
-const Avatar = React.forwardRef<
-  React.ElementRef<typeof AvatarPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
->(({ className, ...props }, ref) => (
-  <AvatarPrimitive.Root
-    ref={ref}
-    className={cn(
-      "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full",
-      className
-    )}
-    {...props}
-  />
-))
-Avatar.displayName = AvatarPrimitive.Root.displayName
+interface AvatarProps {
+  src: string | undefined;
+  alt?: string;
+  variant?: 'default' | 'profile';
+}
 
-const AvatarImage = React.forwardRef<
-  React.ElementRef<typeof AvatarPrimitive.Image>,
-  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>
->(({ className, ...props }, ref) => (
-  <AvatarPrimitive.Image
-    ref={ref}
-    className={cn("aspect-square h-full w-full", className)}
-    {...props}
-  />
-))
-AvatarImage.displayName = AvatarPrimitive.Image.displayName
+export const Avatar = (props: AvatarProps) => {
+  const { src, alt, variant = 'default' } = props;
 
-const AvatarFallback = React.forwardRef<
-  React.ElementRef<typeof AvatarPrimitive.Fallback>,
-  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Fallback>
->(({ className, ...props }, ref) => (
-  <AvatarPrimitive.Fallback
-    ref={ref}
-    className={cn(
-      "flex h-full w-full items-center justify-center rounded-full bg-muted",
-      className
-    )}
-    {...props}
-  />
-))
-AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName
+  let size;
+  switch (variant) {
+    case 'profile':
+      size = 100;
+      break;
 
-export { Avatar, AvatarImage, AvatarFallback }
+    default:
+      size = 32;
+      break;
+  }
+
+  return (
+    <div
+      className={cn(
+        `overflow-hidden flex-none bg-background rounded-full`,
+        variant === 'default' ? 'w-8 h-8 max-h-8' : 'w-28 h-28 max-h-28 border-4 border-background',
+      )}
+    >
+      {!src ? (
+        <Skeleton className={`w-full h-full bg-border`} />
+      ) : (
+        <Image
+          {...props}
+          className={`w-full h-full max-h-full object-cover`}
+          width={size}
+          height={size}
+          src={src}
+          alt={alt || ''}
+          quality={70}
+          priority
+        />
+      )}
+    </div>
+  );
+};
