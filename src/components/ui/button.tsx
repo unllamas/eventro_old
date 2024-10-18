947206e0d49576080776f19b110d8b223326f6db
Jona <llamasjonat@gmail.com>
2024-10-18 00:46:59 -0300

    fix: general changes for UI components

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,9 +18,9 @@ const buttonVariants = cva(
       },
       size: {
         default: 'h-10 px-4 py-2',
-        sm: 'h-8 px-3 text-xs',
+        sm: 'h-8 px-3 text-sm',
         lg: 'h-12 px-4 text-md',
-        icon: 'h-10 w-10',
+        icon: 'h-8 w-8',
       },
     },
     defaultVariants: {
