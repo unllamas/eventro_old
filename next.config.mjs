6fbc730ca4c4526576ab0fad689d4d171646144d
Jona <llamasjonat@gmail.com>
2024-10-18 00:47:16 -0300

    fix(config): integrate env on next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    INSTANT_DB: process.env.INSTANT_DB_APP_ID,
+  },
   images: {
     domains: ['nostr.build', 'static.flickr.com'],
     remotePatterns: [
